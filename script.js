4b97cb2bedee1362e6982b22821d7eb629dc91b7
Syed Darain Hyder Kazmi <darainhyder2004@gmail.com>
2024-08-31 13:32:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function clearDisplay() {
+    document.getElementById('display').innerText = '0';
+}
+
+function deleteLast() {
+    let display = document.getElementById('display').innerText;
+    document.getElementById('display').innerText = display.length > 1 ? display.slice(0, -1) : '0';
+}
+
+function appendCharacter(character) {
+    let display = document.getElementById('display');
+    display.innerText = display.innerText === '0' ? character : display.innerText + character;
+}
+
+function calculateResult() {
+    let display = document.getElementById('display');
+    try {
+        display.innerText = eval(display.innerText);
+    } catch (e) {
+        display.innerText = 'Error';
+    }
+}
